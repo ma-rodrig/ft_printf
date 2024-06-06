cafdc2f831750c51cb06f322532d1f6754bc1342
marodrig <miro.ines@hotmail.com>
2024-06-06 19:16:13 +0100

    printf

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 19:14:02 by marodrig          #+#    #+#             */
+/*   Updated: 2024/06/06 19:15:53 by marodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
