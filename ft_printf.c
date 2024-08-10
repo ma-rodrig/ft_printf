430910de95e46163b1aeea47bbbeb2fb7c41bbf9
marodrig <miro.ines@hotmail.com>
2024-08-10 10:20:43 +0100

    10/8

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,53 @@
 /*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:03:29 by marodrig          #+#    #+#             */
-/*   Updated: 2024/06/12 11:07:30 by marodrig         ###   ########.fr       */
+/*   Updated: 2024/08/10 10:10:53 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	ft_type(va_list arg, char type, int p)
 {
-	
+	if (type == 'c')
+		return (ft_putchar(va_arg(arg, int)));
+	else if (type == 's')
+		return (ft_putstr(va_arg(arg, char *)));
+	else if (type == 'p')
+		return (ft_putptr(va_arg(arg, unsigned long)));
+	else if  (type == 'd' || type == 'i')
+		return (ft_putnbr(va_arg(arg, int)));
+	else if (type == 'u')
+		return (ft_putunsigned(va_arg(arg, unsigned int)));
+	else if (type == 'x' || type == 'X')
+		return (ft_puthex(va_arg(arg, unsigned int), type));
+	else if (type == '%')
+		return(ft_putchar('%'));
+	return (0);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	arg;
+	int		i;
+	int		p;
+
+	i = 0;
+	p = 0;
+	va_start(arg, str);
+	if (!str)
+		return (write(1, NULL, 1));
+	while (str[i])
+	{
+		if (str[i] != '%')
+			p += ft_putchar(str[i]);
+		else
+		{
+			p = ft_type(arg, str[i + 1], p);
+			i++;
+		}
+		i++;
+	}
+	va_end(arg);
+	return (p);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 19:14:02 by marodrig          #+#    #+#             */
-/*   Updated: 2024/06/12 11:22:54 by marodrig         ###   ########.fr       */
+/*   Updated: 2024/08/10 10:16:28 by marodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,11 @@
 # include <stdio.h>
 
 int	ft_printf(const char *format, ...);
+int	ft_putchar(int c);
+int	ft_putstr(char *str);
+int	ft_putptr(unsigned long ptr);
+int	ft_putnbr(int nbr);
+int	ft_putunsigned(unsigned int num);
+int ft_puthex(unsigned int h, const char type);
 
 #endif
