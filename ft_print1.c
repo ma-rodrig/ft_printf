7e7365b4318f3e0981b1922b8c4438bd9f46f265
marodrig <miro.ines@hotmail.com>
2024-08-10 10:32:42 +0100

    ..

---
+++ ft_print1.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print1.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 10:21:58 by marodrig          #+#    #+#             */
+/*   Updated: 2024/08/10 10:30:58 by marodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return ; //needed?
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+int	ft_putptr(unsigned long ptr)
+{
+	;
+}
+int	ft_putnbr(int nbr)
+{
+	if (nbr == -2147483648)
+		ft_putstr("-2147483648");
+	else
+	{
+		if (nbr < 0)
+		{
+			ft_putchar('-');
+			nbr *= -1;
+		}
+		if (nbr > 9)
+			ft_putnbr(nbr / 10);
+		ft_putchar(nbr % 10 + '0');
+	}
+}
---
+++ ft_print2.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marodrig <marodrig@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 10:31:13 by marodrig          #+#    #+#             */
+/*   Updated: 2024/08/10 10:32:12 by marodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putunsigned(unsigned int num)
+{
+	;
+}
+int ft_puthex(unsigned int h, const char type)
+{
+	;
+}
